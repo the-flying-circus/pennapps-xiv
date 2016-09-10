e9c907499c0d870a07a2162ed74e95c48a13bc3f
U-SRKBRADBURY\sbradbury <2017sbradbur@tjhsst.edu>
2016-09-10 00:50:36 -0400

    fix school testing json

--- rawdata/keystone_csv_to_json.py
+++ rawdata/keystone_csv_to_json.py
@@ -6,8 +6,9 @@ print(a[len(a)-2:len(a)])
 
 rd = csv.reader(open('keystone.csv', newline=''))
 wr = open('keystone.json', 'w')
+data = list()
 for row in rd:
     if row[1][len(row[1]) - 2:len(row[1])] == 'SD' and row[4] == '11' and row[5] == 'All Students' and row[7] != 'IS' and row[7] != 'NA':
-        print(json.dumps({'id': int(row[0]), 'name': row[1], 'subject': row[3], 'adv': float(row[7]), 'pro': float(row[8]), 
-            'basic': float(row[9]), 'below': float(row[10])}), wr)
-#json.dump(open('keystone.json', 'w'), fp)
+        data.append(json.dumps({'id': int(row[0]), 'name': row[1], 'subject': row[3], 'adv': float(row[7]), 'pro': float(row[8]), 
+            'basic': float(row[9]), 'below': float(row[10])}))
+json.dump(data, wr)
