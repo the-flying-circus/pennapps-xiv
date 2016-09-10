52e2bda465e6b1d74eba422488777271a0559ec5
Eric Wang <ezwang1@gmail.com>
2016-09-10 00:28:11 -0400

    add haversine

--- data.py
+++ data.py
@@ -4,6 +4,17 @@ import requests
 import json
 import xml.etree.ElementTree as ET
 from secret import ZWSID, GMAPS_API_KEY
+from math import radians, cos, sin, asin, sqrt
+
+def haversine(lon1, lat1, lon2, lat2):
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 3956
+    return c * r
 
 def xml_to_dict(xml):
     out = {}
