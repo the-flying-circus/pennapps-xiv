23f5a5458d4f34f3bf5b907463459a11f75c8b29
Haicheng Charles Zhao <czhao39@gmail.com>
2016-09-10 00:20:04 -0400

    register info url

--- server.py
+++ server.py
@@ -8,6 +8,10 @@ app = Flask(__name__, static_url_path="")
 def index():
     return render_template("index.html")
 
+@app.route("/info")
+def info():
+    return render_template("info.html")
+
 if __name__ == "__main__":
     import sys
     app.run(host="0.0.0.0", port = int(sys.argv[1]) if len(sys.argv) > 1 else 8080, threaded=True)
