cc266310a2a5e0adfadcf4dbad0975f3811943d6
U-SRKBRADBURY\sbradbury <2017sbradbur@tjhsst.edu>
2016-09-10 18:47:31 -0400

    fix school json to include charters

--- rawdata/keystone_csv_to_json.py
+++ rawdata/keystone_csv_to_json.py
@@ -5,7 +5,7 @@ rd = csv.reader(open('keystone.csv', newline=''))
 wr = open('keystone.json', 'w')
 data = list()
 for row in rd:
-    if row[1][len(row[1]) - 2:len(row[1])] == 'SD' and row[4] == '11' and row[5] == 'All Students' and row[7] != 'IS' and row[7] != 'NA':
+    if row[4] == '11' and row[5] == 'All Students' and row[7] != 'IS' and row[7] != 'NA':
         data.append(json.dumps({'id': int(row[0]), 'district': row[1], 'school': row[2], 'subject': row[3], 'adv': row[7], 'pro': row[8], 
             'basic': row[9], 'below': row[10]}))
 json.dump(data, wr)
