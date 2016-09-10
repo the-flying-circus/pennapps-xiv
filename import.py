d968fb489bb14b8429b82c418f8f852ce23ac159
Eric Wang <ezwang1@gmail.com>
2016-09-10 03:09:30 -0400

    stuff

--- import.py
+++ import.py
@@ -6,46 +6,51 @@ import pymongo
 from datetime import datetime
 from secret import DB_URL
 
+do_crime = True
+do_collisions = True
+
 crime_file = "../crime_philadelphia.csv"
 collisions_file = "../collisions_philadelphia.csv"
 
 client = pymongo.MongoClient(DB_URL)
 db = client["homie"]
 
-crime = db["crime"]
-crime.drop()
-crime.create_index([("coord", pymongo.GEO2D)])
+if do_crime:
+    crime = db["crime"]
+    crime.drop()
+    crime.create_index([("coord", pymongo.GEO2D)])
 
-with open(crime_file) as f:
-    reader = csv.reader(f)
-    print(next(reader, None))
-    count = 0
-    for row in reader:
-        if count > 1000000:
-            break
-        loc = row[-2]
-        if not loc.startswith("POINT"):
-            continue
-        loc = [float(x) for x in loc[7:-1].split(" ")]
-        crime.insert_one({
-            "coord": loc,
-            "type": row[-3],
-            "time": datetime.strptime(row[2], "%m/%d/%Y %I:%M:%S %p")
-        })
-        count += 1
+    with open(crime_file) as f:
+        reader = csv.reader(f)
+        print(next(reader, None))
+        count = 0
+        for row in reader:
+            if count > 1000000:
+                break
+            loc = row[-2]
+            if not loc.startswith("POINT"):
+                continue
+            loc = [float(x) for x in loc[7:-1].split(" ")]
+            crime.insert_one({
+                "coord": loc,
+                "type": row[-3],
+                "time": datetime.strptime(row[2], "%m/%d/%Y %I:%M:%S %p")
+            })
+            count += 1
 
-collisions = db["collisions"]
-collisions.drop()
-collisions.create_index([("coord", pymongo.GEO2D)])
+if do_collisions:
+    collisions = db["collisions"]
+    collisions.drop()
+    collisions.create_index([("coord", pymongo.GEO2D)])
 
-with open(collisions_file) as f:
-    reader = csv.reader(f)
-    print(next(reader, None))
-    for row in reader:
-        collisions.insert_one({
-            "coord": [float(row[0]), float(row[1])],
-            "year": int(row[4]),
-            "month": int(row[5])
-        })
+    with open(collisions_file) as f:
+        reader = csv.reader(f)
+        print(next(reader, None))
+        for row in reader:
+            collisions.insert_one({
+                "coord": [float(row[0]), float(row[1])],
+                "year": int(row[4]),
+                "month": int(row[5])
+            })
 
 client.close()
