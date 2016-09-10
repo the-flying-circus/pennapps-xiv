785b9112f23bc49e13dfd96c6d4c081094f5c2b4
Eric Wang <ezwang1@gmail.com>
2016-09-10 01:23:27 -0400

    add db url

--- secret.py
+++ secret.py
@@ -2,5 +2,6 @@
 # Use a tool to remove it completely and force push.
 # Add an secret.py.example file with necessary fields.
 
+DB_URL = "mongodb://user:qRaTNKHpg33utHdZhmVP3rXhtVJ3TrF7Nd5tztA2@ds019688.mlab.com:19688/homie"
 GMAPS_API_KEY = "AIzaSyDvNIS8JcGJ6HOB-EQYefrl_J82qDthDCA"
 ZWSID = "X1-ZWz19kxdvgpc0b_3jtqm"
