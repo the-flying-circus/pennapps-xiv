c864a214df27cbee6252328ebcca1de04343fac5
U-SRKBRADBURY\sbradbury <2017sbradbur@tjhsst.edu>
2016-09-10 01:12:41 -0400

    fix school data for city district

--- rawdata/keystone_csv_to_json.py
+++ rawdata/keystone_csv_to_json.py
@@ -1,14 +1,11 @@
 import json
 import csv
 
-a = 'one'
-print(a[len(a)-2:len(a)])
-
 rd = csv.reader(open('keystone.csv', newline=''))
 wr = open('keystone.json', 'w')
 data = list()
 for row in rd:
     if row[1][len(row[1]) - 2:len(row[1])] == 'SD' and row[4] == '11' and row[5] == 'All Students' and row[7] != 'IS' and row[7] != 'NA':
-        data.append(json.dumps({'id': int(row[0]), 'name': row[1], 'subject': row[3], 'adv': float(row[7]), 'pro': float(row[8]), 
+        data.append(json.dumps({'id': int(row[0]), 'district': row[1], 'school': row[2], 'subject': row[3], 'adv': float(row[7]), 'pro': float(row[8]), 
             'basic': float(row[9]), 'below': float(row[10])}))
 json.dump(data, wr)
