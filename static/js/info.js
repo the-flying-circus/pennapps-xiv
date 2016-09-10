72fc0f7be75030a448e7ea5dd6173f92b8c9ec37
Haicheng Charles Zhao <czhao39@gmail.com>
2016-09-10 13:37:11 -0400

    faster scroll reveal

--- static/js/info.js
+++ static/js/info.js
@@ -2,5 +2,5 @@ $(document).ready(function() {
     $(".timeago").timeago();
     
     var sr = ScrollReveal();
-    sr.reveal(".property-img, .map, .card", {duration: 1000});
+    sr.reveal(".property-img, .map, .card");
 });
